8d132f25eb4fc436baa8c059789431f5b1b34b30
giswqs <giswqs@gmail.com>
2026-08-20 16:31:12 -0400

    Use direct COPC clip downloads

--- packages/plugins/src/plugins/maplibre-usgs-lidar.ts
+++ packages/plugins/src/plugins/maplibre-usgs-lidar.ts
@@ -131,7 +131,7 @@ const mountUsgsLidarControl = (app: GeoLibreAppAPI): boolean => {
 export const maplibreUsgsLidarPlugin: GeoLibrePlugin = {
   id: "maplibre-gl-usgs-lidar",
   name: "USGS LiDAR",
-  version: "0.11.2",
+  version: "0.11.3",
   activate: (app: GeoLibreAppAPI) => {
     pluginActive = true;
 
